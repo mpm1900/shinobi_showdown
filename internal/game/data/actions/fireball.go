0fc7a92c394b6c3fd64ec19616fb49b79f1384e1
mpm1900 <almostmacmiller@gmail.com>
2026-05-28 12:36:55 -0500

    lead by examle: fireball

--- internal/game/data/actions/fireball.go
+++ internal/game/data/actions/fireball.go
@@ -10,20 +10,18 @@ import (
 var Fireball = MakeFireball()
 
 func MakeFireball() game.Action {
-	config := makeAttackConfig(game.ActionConfig{
-		Name:        "Fireball",
-		Description: "10% chance to burn target.",
-		Nature:      game.Ptr(game.NsFire),
-		Accuracy:    game.Ptr(100),
-		Power:       game.Ptr(55),
-		Stat:        game.Ptr(game.StatChakraAttack),
-		Cost:        game.Ptr(40),
-		Jutsu:       game.Ninjutsu,
-	})
-
 	return makeAttack(AttackConfig{
-		ID:     uuid.MustParse("aaf5174b-f386-54b1-84c4-0c062937c770"),
-		Config: config,
+		ID: uuid.MustParse("aaf5174b-f386-54b1-84c4-0c062937c770"),
+		Config: makeAttackConfig(game.ActionConfig{
+			Name:        "Fireball",
+			Description: "10% chance to burn target.",
+			Nature:      game.Ptr(game.NsFire),
+			Accuracy:    game.Ptr(100),
+			Power:       game.Ptr(55),
+			Stat:        game.Ptr(game.StatChakraAttack),
+			Cost:        game.Ptr(40),
+			Jutsu:       game.Ninjutsu,
+		}),
 		OnSuccess: func(g game.Game, _, context game.Context, action_config game.ActionConfig) []game.GameTransaction {
 			transactions := game.NewTransactionBuilder()
 			targets := g.GetTargets(context)
